2e7caf6a3426c2f8cab8e166b66f4ce1dd3c7ec9
take-cantik <miki.takeru508@mail.kyutech.jp>
2020-05-23 20:36:45 +0900

    Change message aiueo

--- main.py
+++ main.py
@@ -41,7 +41,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = event.message.text
+    message = "あいうえお"
+    # message = event.message.text
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=message))

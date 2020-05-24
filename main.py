56dbcd7008ff3055745b0fc32b21a8b634473f37
take-cantik <miki.takeru508@mail.kyutech.jp>
2020-05-24 20:35:19 +0900

    Add def is_finish

--- main.py
+++ main.py
@@ -21,6 +21,12 @@ LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
+def is_finish(finish_num):
+    if finish_num == 0:
+        return 1
+    else:
+        return 0
+
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -42,6 +48,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     bingolist = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    number = 0:
 
     if event.message.text == "スタート":
         message = "ビンゴ"
@@ -54,6 +61,8 @@ def handle_message(event):
 
     elif "説明" in event.message.text:
         message = "ビンゴの説明"
+    elif is_finish(number) == 1:
+        message = "終了です"
     else:
         message = "散歩ビンゴです。開始したい時は「スタート」やり方を知りたい時は「説明」と打ってね。"
 

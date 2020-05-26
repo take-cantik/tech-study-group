f4e261893208866c90bd78c55086d3b3526da319
take-cantik <miki.takeru508@mail.kyutech.jp>
2020-05-26 22:25:11 +0900

    path

--- test2.py
+++ test2.py
@@ -58,8 +58,8 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         ImageSendMessage(
-            original_content_url = "https://teruteruahuro.herokuapp.com/images/opnecv_concat_tile.jpg",
-            preview_image_url = "https://teruteruahuro.herokuapp.com/images/opencv_cancat_tile.jpg"
+            original_content_url = "https://teruteruahuro.herokuapp.com/dst/opnecv_concat_tile.jpg",
+            preview_image_url = "https://teruteruahuro.herokuapp.com/dst/opencv_cancat_tile.jpg"
         )
     )
 

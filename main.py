2dcb95cc5adfab86cba6cc329f604b7e44f7a91b
take-cantik <miki.takeru508@mail.kyutech.jp>
2020-05-26 11:54:22 +0900

    Mix main and test

--- main.py
+++ main.py
@@ -15,7 +15,11 @@ import random
 import cv2
 import numpy as np
 
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 #環境変数取得
 LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
@@ -24,7 +28,23 @@ LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-def is_finish(finish_num):
+#クラス指定
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_num = db.Column(db.Integer, unique=False)
+
+    def __init__(self, user_num):
+        self.user_num = user_num
+
+class Bingo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    bingo_num = db.Column(db.Integer, unique=False)
+
+    def __init__(self, bingo_num):
+        self.bingo_num = bingo_num
+
+#関数指定
+def is_bingo(finish_num):
     if finish_num == 0:
         return 1
     else:
@@ -50,39 +70,79 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    bingolist = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    bingolist = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-    >>> d = {'配達員':1,'タピオカ':2,'噴水':3,'密':4,'ジョギング':5,'パトカー':6,'カップル':7,'鳶':8,'黒マスク':9}
-    >>> d
-    {'配達員':1,'タピオカ':2,'噴水':3,'密':4,'ジョギング':5,'パトカー':6,'カップル':7,'鳶':8,'黒マスク':9}
-    >>> type(d)
-    <class 'dict'>
+    userNum = db.session.query(User).all()
+    num = userNum[-1].user_num
 
-    number = 0
+    numnum = 0
 
-    if event.message.text == "スタート":
-        message = "ビンゴ"
+    if "終了" in event.message.text:
+        message = "終了です\n"
+
+        if is_bingo(numnum) == 1:
+            message += "ビンゴです！"
+
+        number = 0
+    elif "スタート" in event.message.text:
         random.shuffle(bingolist)
 
-        for num in bingolist:
-            if bingolist.index(num) % 3 == 0:
-                message += "\n"
-            message += num
+        message = "ビンゴ\n"
+        n = 0
 
-    elif event.message.text in bingolist:
-        bingolist[bingolist.index(event.message.text)] = "0"
-        message = "ビンゴ"
-        for num in bingolist:
-            if bingolist.index(num) % 3 == 0:
-                message += "\n"
-            message += num
+        for bin_num1 in bingolist:
 
+            if n % 3 == 0:
+                message += "\n"
+
+            message += str(bin_num1)
+            n += 1
+
+        for bin_num2 in reversed(bingolist):
+            bingo = Bingo(bin_num2)
+            db.session.add(bingo)
+            db.session.commit()
+
+        number = 1
     elif "説明" in event.message.text:
         message = "ビンゴの説明"
-    elif is_finish(number) == 1:
-        message = "終了です"
+    elif num == 1:
+        bingolist = db.session.query(Bingo).all()
+        message = "ビンゴ\n"
+        binlis = []
+        n = 0
+        m = 0
+        for bin_num in reversed(bingolist):
+            if n % 3 == 0:
+                message += "\n"
+
+            message += str(bin_num.bingo_num)
+            binlis.append(bin_num.bingo_num)
+            n += 1
+
+            if n == 9:
+                break
+
+        for i in binlis:
+            if str(i) == event.message.text:
+                binlis[binlis.index(i)] = 0
+            m = 1
+
+        if m == 1:
+            for bin_num2 in reversed(binlis):
+            bingo = Bingo(bin_num2)
+            db.session.add(bingo)
+            db.session.commit()
+
+        number = 1
     else:
         message = "散歩ビンゴです。開始したい時は「スタート」やり方を知りたい時は「説明」と打ってね。"
+        number = 0
+
+    num = number
+    user = User(num)
+    db.session.add(user)
+    db.session.commit()
 
     line_bot_api.reply_message(
         event.reply_token,
